118d2238fbf879d4d0f89f10b0f8b0ef1c2b375c
prasad <psam165@gmail.com>
2016-03-09 16:29:40 -0500

    CXF logging is enabled and now writes through log4j

--- src/main/java/javax/cxf/service/GreetingService.java
+++ src/main/java/javax/cxf/service/GreetingService.java
@@ -7,21 +7,17 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.xml.ws.BindingType;
 
+import org.apache.log4j.Logger;
+
 @WebService
 @BindingType(value=javax.xml.ws.soap.SOAPBinding.SOAP12HTTP_BINDING)
 public class GreetingService {
 	
-
-
-	/*public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Publishing....");
-		Endpoint.publish("http://localhost:8084/Greet", new GreetingService());
-
-	}*/
+private static Logger log4j = Logger.getLogger(GreetingService.class);
 	
 	@WebMethod
 	public String greet(){
+		log4j.info("Just checking......log4j");
 		try {
 			TimeUnit.SECONDS.sleep(5);
 		} catch (InterruptedException e) {
